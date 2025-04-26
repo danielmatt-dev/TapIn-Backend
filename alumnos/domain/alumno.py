10ca03516bfd5b07c636a3b4542f540badeaa7ff
Daniel <danielmatt1701@gmail.com>
2025-04-25 22:20:49 -0600

    Actualizar domain, DTOs y Serializer
    - Se actualizaron las propiedades de la clase Alumno
    - Se añadieron el resto de campos en AlumnoDTO
    - Se añadieron todos los campos para serializar los datos en
      AlumnoSerializer

--- alumnos/domain/alumno.py
+++ alumnos/domain/alumno.py
@@ -1,6 +1,15 @@
 from dataclasses import dataclass
+from datetime import date
 
 
 @dataclass
 class Alumno:
-    nombre: str
+    id_alumno: str
+    nombre_completo: str
+    curp: str
+    sexo: str
+    correo_institucional: str
+    fecha_nacimiento: date
+    telefono_tutor: str
+    es_silenciado: bool
+    estado: str
