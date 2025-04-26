9cc7752f12be9f49ba1c37f2b990f53a51b526a8
Daniel <danielmatt1701@gmail.com>
2025-04-25 22:23:05 -0600

    Implementar mapeo de entidades
    - Se implementó el mapeo de entidades de domain a model, model a domain
      y domain a DTO

--- alumnos/infrastructure/mapper/alumno_mapper.py
+++ alumnos/infrastructure/mapper/alumno_mapper.py
@@ -1,15 +1,20 @@
 from abc import ABC, abstractmethod
 
 from alumnos.domain.alumno import Alumno
+from alumnos.domain.dtos import AlumnoDTO
 from alumnos.infrastructure.alumno_model import AlumnoModel
 
 
 class AlumnoMapper(ABC):
 
     @abstractmethod
-    def toDomain(self, model: AlumnoModel) -> Alumno:
+    def to_domain(self, model: AlumnoModel) -> Alumno:
         pass
 
     @abstractmethod
-    def toModel(self, domain: Alumno) -> AlumnoModel:
+    def to_model(self, domain: Alumno) -> AlumnoModel:
+        pass
+
+    @abstractmethod
+    def to_dto(self, domain: Alumno) -> AlumnoDTO:
         pass
--- alumnos/infrastructure/mapper/alumno_mapper_impl.py
+++ alumnos/infrastructure/mapper/alumno_mapper_impl.py
@@ -1,16 +1,45 @@
-from abc import ABC
-
 from alumnos.domain.alumno import Alumno
+from alumnos.domain.dtos import AlumnoDTO
 from alumnos.infrastructure.alumno_model import AlumnoModel
 
 from alumnos.infrastructure.mapper.alumno_mapper import AlumnoMapper
 
 
-class AlumnoMapperImpl(AlumnoMapper, ABC):
+class AlumnoMapperImpl(AlumnoMapper):
 
-    def toDomain(self, model: AlumnoModel) -> Alumno:
-        return Alumno(model.nombre_completo)
+    def to_domain(self, model: AlumnoModel) -> Alumno:
+        return Alumno(
+            id_alumno=model.id_alumno,
+            nombre_completo=model.nombre_completo,
+            curp=model.curp,
+            sexo=model.sexo,
+            correo_institucional=model.correo_institucional,
+            fecha_nacimiento=model.fecha_nacimiento,
+            telefono_tutor=model.telefono_tutor,
+            es_silenciado=model.es_silenciado,
+            estado=model.estado
+        )
 
-    def toModel(self, domain: Alumno) -> AlumnoModel:
-        return AlumnoModel(domain.nombre)
+    def to_model(self, domain: Alumno) -> AlumnoModel:
+        return AlumnoModel(
+            id_alumno=domain.id_alumno,
+            nombre_completo=domain.nombre_completo,
+            curp=domain.curp,
+            sexo=domain.sexo,
+            correo_institucional=domain.correo_institucional,
+            fecha_nacimiento=domain.fecha_nacimiento,
+            telefono_tutor=domain.telefono_tutor,
+            es_silenciado=domain.es_silenciado,
+            estado=domain.estado
+        )
 
+    def to_dto(self, domain: Alumno) -> AlumnoDTO:
+        return AlumnoDTO(
+            id_alumno=domain.id_alumno,
+            nombre_completo=domain.nombre_completo,
+            curp=domain.curp,
+            sexo=domain.sexo,
+            correo_institucional=domain.correo_institucional,
+            fecha_nacimiento=domain.fecha_nacimiento,
+            telefono_tutor=domain.telefono_tutor
+        )
